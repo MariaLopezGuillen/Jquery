6753875e38b8a0537d3f0bfa04d0409be2962b2e
Maria <mariialg21@alumnos.ilernasevilla.com>
2024-01-22 13:30:20 +0100

    captcha

--- js/contact.js
+++ js/contact.js
@@ -1,25 +1,37 @@
-$(document).ready(function(){
-    $('input').focusin(function(){
-    // Cuando el campo de texto obtiene el foco
-    $(this).css({borderColor: '#ff0000', backgroundColor: '#ffffcc'});
-    }).focusout(function(){
-    // Cuando el campo de texto pierde el foco
-    $(this).css({borderColor: '', backgroundColor: ''});
+$(document).ready(function () {
+    $('input').focusin(function () {
+        // Cuando el campo de texto obtiene el foco
+        $(this).css({ borderColor: '#ff0000', backgroundColor: '#ffffcc' });
+    }).focusout(function () {
+        // Cuando el campo de texto pierde el foco
+        $(this).css({ borderColor: '', backgroundColor: '' });
     });
-   });
-   
-   $(document).ready(function(){
-    $('.empresa').hover(function(){
-     // Cuando el ratón entra en la imagen
-     $(this).find('.normal').hide();
-     $(this).find('.hover').show();
-    }, function(){
-     // Cuando el ratón sale de la imagen
-     $(this).find('.normal').show();
-     $(this).find('.hover').hide();
-    });
-   });
+});
+//captcha
+$('#validar').click(function() {
+    var usuarioCaptcha = $('#usuarioCaptcha').val();
+    if(usuarioCaptcha == captcha) {
+        alert("¡Correcto!");
+    } else {
+        alert("¡Incorrecto!");
+    }
+});
 
-   $("#botonEnvio").click(function() {
+   
+//----
+
+$(document).ready(function () {
+    $('.empresa').hover(function () {
+        // Cuando el ratón entra en la imagen
+        $(this).find('.normal').hide();
+        $(this).find('.hover').show();
+    }, function () {
+        // Cuando el ratón sale de la imagen
+        $(this).find('.normal').show();
+        $(this).find('.hover').hide();
+    });
+});
+
+$("#botonEnvio").click(function () {
     alert("¡Formulario enviado!");
 });
